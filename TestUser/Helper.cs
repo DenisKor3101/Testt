472d8a478389bcf0437e3f62508fa3d95abb775e
Роман Немилов <roma02696@gmail.com>
2022-12-08 09:51:28 +0300

    Добавьте файлы проекта.

---
+++ TestUser/Helper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestUser
+{
+    public class Helper
+    {
+        public static Entity.DBAutorization02Entities DB { get; set; }
+    }
+}
---
+++ TestUser/View/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace TestUser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Helper.DB = new Entity.DBAutorization02Entities();
+            //List<Entity.User> users = Helper.DB.User.ToList();
+            //dataGridView1.DataSource = users;
+
+            var users = Helper.DB.User.Select(x => new {x.Login, x.Password, x.Role.RoleName }).ToList();
+            dataGridView1.DataSource = users;
+        }
+    }
+}
